16e34d71486d6c262d6e6d407d5b7da1611cb353
HollTil <detilman83@gmail.com>
2023-07-12 16:00:54 +0200

    Lib imported Main, Mail, Sender, SMTPConnection completed and tested

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import Mail.Mail;
+import Sender.Sender;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        test();
+    }
+    private static void test(){
+        Mail mail = new Mail();
+
+        mail.setRecipient("detilman83@gmail.com");
+        mail.setSubject("Test mit JAVA Mail App");
+        mail.setMessage("Hallo, jetzt haben Sie auch eine Email von mir...");
+
+        try {
+            Sender sender = new Sender();
+           // for (int i = 0; i<100; i++) {  <= for-Schleife für vielfaches Senden an eine Email-Adresse
+                sender.send(mail);
+           // }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
--- src/Sender/Sender.java
+++ src/Sender/Sender.java
@@ -1,4 +1,50 @@
 package Sender;
+import Mail.Mail;
+import SMTP.SMTPConnection;
+import javax.mail.*;
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.*;
 
 public class Sender {
+    private SMTPConnection connection;
+    private Session session;
+    public Sender() throws Exception {
+        this.connection = new SMTPConnection();
+        try {
+            Session session = this.connection.getConnection();
+        } catch (Exception ex){
+            System.out.println("LOG: " + ex.getMessage());
+            throw new Exception("Connection failed");
+        }
+    }
+
+    public boolean send(Mail mail) throws AddressException, MessagingException, Exception {
+
+
+
+    Message message = new MimeMessage(session);
+    message.setFrom(new InternetAddress(this.connection.getFrom()));
+
+    message.setRecipients(
+            Message.RecipientType.TO, InternetAddress.parse(mail.getRecipient()));
+
+    message.setSubject(mail.getSubject());
+
+    // Content besteht aus Bodyparts = text, anhang etc wird zusammengefasst als Content
+    MimeBodyPart mimeBodyPart = new MimeBodyPart();
+    mimeBodyPart.setContent(mail.getMessage(), "text/html; charset=utf-8");
+
+    Multipart multipart = new MimeMultipart();
+    multipart.addBodyPart(mimeBodyPart);
+
+    message.setContent(multipart);
+    try {
+    Transport.send(message);
+    } catch (Exception ex ){
+        throw new Exception("Cannot send Message!");
+    }
+        return true;
+    }
 }
