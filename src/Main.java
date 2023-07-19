b3f1e0d580e63da851b412db433bcc7c95a76f25
HollTil <detilman83@gmail.com>
2023-07-19 15:07:51 +0200

    scanner in TerminalUI.java implemented TerminalUI.java in main integrated Tested-by: HolTil

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import Mail.Mail;
 import Sender.Sender;
+import UI.TerminalUI;
 
 public class Main {
     public static void main(String[] args) {
-        test();
+        //test();
+        try {
+            TerminalUI.runTerminal();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
     private static void test(){
         Mail mail = new Mail();
--- src/UI/TerminalUI.java
+++ src/UI/TerminalUI.java
@@ -1,4 +1,36 @@
 package UI;
 
+import Mail.Mail;
+import Sender.Sender;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class TerminalUI {
+    public static void runTerminal() throws Exception {
+        String recipient;
+        String subject;
+        String message;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bitte gib die Email des Empfängers ein: ");
+        recipient = scanner.nextLine();
+
+
+        System.out.println("Bitte gib einen Betreff an: ");
+        subject = scanner.nextLine();
+
+
+        System.out.println("Bitte gib deine Nachricht ein: ");
+        message = scanner.nextLine();
+
+        Mail mail = new Mail();
+        mail.setRecipient(recipient);
+        mail.setSubject(subject);
+        mail.setMessage(message);
+
+        Sender sender = new Sender();
+        sender.send(mail);
+    }
 }
